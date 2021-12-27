511a139b2804dea6610937a32df72b8d13528b51
ashtanko <shtankopro@gmail.com>
2021-12-27 12:45:25 +0200

    Add google maps

--- lib/ui/screen/google_maps_screen.dart
+++ lib/ui/screen/google_maps_screen.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
-class GoogleMapsScreen extends StatelessWidget {
+class GoogleMapsScreen extends StatefulWidget {
   const GoogleMapsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GoogleMapsScreen> createState() => _GoogleMapsScreenState();
+}
+
+class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +29,13 @@ class GoogleMapsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Google Maps TODO'),
       ),
-      body: Container(),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
